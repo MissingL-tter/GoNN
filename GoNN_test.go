8ab6545cc4a39a41424e814095f5cfd589a019af
Daniel Lawrence <64.74.6c@gmail.com>
2017-07-28 01:56:55 -0400

    Updated testing file

--- GoNN_test.go
+++ GoNN_test.go
@@ -9,6 +9,13 @@ func TestGoNN(t *testing.T) {
 	var n = CreateNetwork(2, 2, []int{3, 3}, 1)
 
 	for i := 0; i < len(n.Weights); i++ {
+		if i == 0 {
+			print(len(n.Weights[i]))
+			println(" input nodes with weights: ")
+		} else if i < len(n.Weights) {
+			print(len(n.Weights[i]))
+			println(" hidden nodes with weights: ")
+		}
 		for j := 0; j < len(n.Weights[i]); j++ {
 			for k := 0; k < len(n.Weights[i][j]); k++ {
 				print(int(n.Weights[i][j][k]))
