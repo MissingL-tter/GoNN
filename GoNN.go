5f57101f26ca663c7acd05ab3797e42fce9ca4ff
MissingL-tter <434a44@gmail.com>
2017-08-13 20:04:31 -0400

    Completely overhauled the structure of the network.
    The goal of this project is to provide a simple package that will allow users to easily understand the structure of a neural network. Although I will be losing some performance in training, I believe this approach is much more clear.

--- GoNN.go
+++ GoNN.go
@@ -1,101 +1,176 @@
 package GoNN
 
 import (
+	"errors"
+	"fmt"
 	"math"
 	"math/rand"
-	"time"
 )
 
 // Network defines the structure of the network
 type Network struct {
-	Biases  [][]float64
-	Weights [][]float64
+	Layers [][]*Node
+}
+
+// Node defines a node in the network
+type Node struct {
+	In   []*Edge
+	Bias float64
+	Out  []*Edge
+}
+
+// Edge defines a connection between nodes in the network
+type Edge struct {
+	Start  *Node
+	Weight float64
+	End    *Node
 }
 
 // CreateNetwork returns a Network struct.
 //
-// inputNodes is the number of inputs, hiddenLayers is the number of hidden layers, hiddenNodes is an array representing the number of nodes for each hidden layer.
-func CreateNetwork(inputNodes int, hiddenLayers int, hiddenNodes []int, outputNodes int) *Network {
+// layers is the description of the network such that CreateNetwork(5, 3, 2) returns a network with 5 input nodes, 1 hidden layer with 3 nodes, and 2 output nodes
+// layers requires at least two inputs but a variadic has been used for clarity in building the network
+func CreateNetwork(nodeCounts ...int) (*Network, error) {
 
-	// Init layers and fill with for the number of nodes at that layer
-	biases := make([][]float64, hiddenLayers+2)
-
-	biases[0] = make([]float64, inputNodes)
-	biases[len(biases)-1] = make([]float64, outputNodes)
-
-	for i := range hiddenNodes {
-		biases[i+1] = make([]float64, hiddenNodes[i])
+	// First verify that we have at least input and output nodes for out network.
+	if len(nodeCounts) < 2 {
+		return nil, errors.New("Cannot build an empty network. CreateNetwork() must have at least 2 parameters")
+	}
+	for _, l := range nodeCounts {
+		if l < 1 {
+			return nil, errors.New("Cannot build a layer of zero nodes. All parameters must be greater than 0")
+		}
 	}
 
-	// Init weights and fill with weights for each node at each layer
-	weights := make([][]float64, len(biases)-1)
+	// Create an array to represent each layer and
+	layers := make([][]*Node, len(nodeCounts))
 
-	for i := range weights {
-		weights[i] = make([]float64, len(biases[i])*len(biases[i+1]))
-	}
+	// for each layer make the array for the nodes in that layer.
+	for l := range layers {
+		layers[l] = make([]*Node, nodeCounts[l])
 
-	return &Network{biases, weights}
-}
-
-// InitDebug initializes the network where all biases = 1 and all weights = 2
-func (n Network) InitDebug() {
-
-	for i := range n.Biases {
-		if i != 0 {
-			for j := range n.Biases[i] {
-				n.Biases[i][j] = 1
-			}
-			for j := range n.Weights[i-1] {
-				n.Weights[i-1][j] = 2
+		// Initialize each node's bias from a normal distribution
+		for n := range layers[l] {
+			if l != 0 {
+				layers[l][n] = &Node{nil, rand.NormFloat64(), nil}
+			} else {
+				// unless they are input nodes, which get initialized at 0.
+				layers[l][n] = &Node{nil, 0, nil}
 			}
 		}
 	}
 
-}
+	// In every layer that is not an input,
+	for l := 1; l < len(layers); l++ {
 
-// Init initializes the network with normally distributed biases adn weights
-func (n Network) Init() {
+		// for each node in that layer
+		for n := range layers[l] {
 
-	rand.Seed(time.Now().Unix())
-
-	for i := range n.Biases {
-		if i != 0 {
-			for j := range n.Biases[i] {
-				n.Biases[i][j] = rand.NormFloat64()
-			}
-			for j := range n.Weights[i-1] {
-				n.Weights[i-1][j] = rand.NormFloat64()
+			// create an edge to each node in the layer before it and initialize its weight from a normal distribution
+			for k := range layers[l-1] {
+				edge := &Edge{layers[l-1][k], rand.NormFloat64(), layers[l][n]}
+				layers[l-1][k].Out = append(layers[l-1][k].Out, edge)
+				layers[l][n].In = append(layers[l][n].In, edge)
 			}
 		}
 	}
+
+	// Return a pointer to the network
+	return &Network{layers}, nil
 }
 
-// ForwardProp takes some input and processes it through the network to return an output
-// Basic concept implemented
-func (n Network) ForwardProp() []float64 {
+// createNetworkDebug returns a Network struct where every bias is initialized at 1, and every weight at 2
+func createNetworkDebug(nodeCounts ...int) (*Network, error) {
 
-	input := n.Biases[0]
-
-	for l := 1; l < len(n.Biases); l++ {
-		var output []float64
-		for b := 0; b < len(n.Biases[l]); b++ {
-			sum := 0.0
-			for i := range input {
-				idx := len(input)*b + i
-				sum += input[i] * n.Weights[l-1][idx]
-			}
-			output = append(output, sigmoid(sum+n.Biases[l][b]))
+	// First verify that we have at least input and output nodes for out network.
+	if len(nodeCounts) < 2 {
+		return nil, errors.New("Cannot build an empty network. CreateNetwork() must have at least 2 parameters")
+	}
+	for _, l := range nodeCounts {
+		if l < 1 {
+			return nil, errors.New("Cannot build a layer of zero nodes. All parameters must be greater than 0")
 		}
-		input = output
 	}
 
-	return input
+	// Create an array to represent each layer and
+	layers := make([][]*Node, len(nodeCounts))
+
+	// for each layer make the array for the nodes in that layer.
+	for l := range layers {
+		layers[l] = make([]*Node, nodeCounts[l])
+
+		// Initialize each node's bias at 1
+		for n := range layers[l] {
+			if l != 0 {
+				layers[l][n] = &Node{nil, 1, nil}
+			} else {
+				// unless they are input nodes, which get initialized at 0.
+				layers[l][n] = &Node{nil, 0, nil}
+			}
+		}
+	}
+
+	// In every layer that is not an input,
+	for l := 1; l < len(layers); l++ {
+
+		// for each node in that layer
+		for n := range layers[l] {
+
+			// create an edge to each node in the layer before it and initialize its weight at 2
+			for k := range layers[l-1] {
+				edge := &Edge{layers[l-1][k], 2, layers[l][n]}
+				layers[l-1][k].Out = append(layers[l-1][k].Out, edge)
+				layers[l][n].In = append(layers[l][n].In, edge)
+			}
+		}
+	}
+
+	// Return a pointer to the network
+	return &Network{layers}, nil
+}
+
+// ForwardPropagate takes some input and processes it through the network to return the output
+//
+// The output returned will be an array of size corresponding to the number of nodes in the output layer
+func (net Network) ForwardPropagate(layerIn []float64) ([]float64, error) {
+
+	// Verify that we have enough inputs to satisfy the input layer
+	if len(layerIn) != len(net.Layers[0]) {
+		return nil, fmt.Errorf("Length of input array must be the length of the input layer. Expected:%v Got:%v", len(net.Layers[0]), len(layerIn))
+	}
+
+	// For each layer that is not the input layer
+	for _, layer := range net.Layers[1:] {
+
+		// create an array to store its output
+		layerOut := []float64{}
+
+		// and for each node in that layer
+		for _, node := range layer {
+
+			// calculate the dot product of all incoming nodes, and their edges weights.
+			dotProd := 0.
+			for e, edge := range node.In {
+				dotProd += layerIn[e] * edge.Weight
+			}
+
+			// Then add the node's bias to the dot product, apply the sigmoid function,
+			// and store the result in the layer's output
+			layerOut = append(layerOut, sigmoid(dotProd+node.Bias))
+		}
+
+		// At the end of the layer, set the input for the next layer as this layer's output.
+		layerIn = layerOut
+	}
+
+	// Return the final output once we have traversed all layers
+	return layerIn, nil
 
 }
 
-// BackProp takes some output compared to what was expected by the input and uses stochastic gradient descent to minimize the loss function
-func (n Network) BackProp() []float64 {
-	return []float64{0.0, 0.0}
+// BackPropagate takes some output compared to what was expected by the input and uses stochastic gradient descent to minimize the loss function
+func (net Network) BackPropagate() []float64 {
+	return []float64{0.}
 }
 
 // Stochastic Gradient Descent
@@ -103,12 +178,14 @@ func sgd() {
 
 }
 
+// The activation function used to squash the input to between 0 and 1
 func sigmoid(z float64) float64 {
 
 	return 1 / (1 + math.Exp(-z))
 
 }
 
+// The derivative of the sigmoid function
 func sigPrime(z float64) float64 {
 
 	return sigmoid(z) * (1 - sigmoid(z))
--- GoNN_test.go
+++ GoNN_test.go
@@ -2,64 +2,97 @@ package GoNN
 
 import (
 	"fmt"
+	"os"
 	"testing"
+	"text/tabwriter"
 )
 
 func TestNetworkStruct(t *testing.T) {
 
-	n := CreateNetwork(2, 1, []int{1}, 1)
-	n.InitDebug()
+	structure := []int{2, 3, 2}
 
-	for i := range n.Weights {
+	n, err := createNetworkDebug(structure...)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	fmt.Print("Testing on:")
+	for _, i := range structure {
+		fmt.Printf(" %v", i)
+	}
+	fmt.Println()
+
+	for i := range n.Layers {
+		if len(n.Layers[i]) != structure[i] {
+			t.Errorf("Incorrect size of Layer %v | Have: %v | Want: %v", i, len(n.Layers[i]), structure[i])
+		}
+	}
+
+	if !t.Failed() {
+		fmt.Println("No errors found. All layers correctly sized.")
+	}
+
+}
+
+func TestNetworkPrint(t *testing.T) {
+
+	n, err := createNetworkDebug(2, 3, 2)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 0, ' ', tabwriter.Debug)
+	fmt.Fprintf(writer, "Nodes \t Bias \t Weights \t \n")
+	fmt.Fprint(writer, "\t \t \t \n")
+
+	for i := range n.Layers {
 
 		if i == 0 {
-			fmt.Print(len(n.Biases[i]))
-			fmt.Printf("%25s", " input nodes with bias: ")
-			for k := range n.Biases[i] {
-				fmt.Printf("%+.2f ", n.Biases[i][k])
+			fmt.Fprintf(writer, "%v Input ", len(n.Layers[i]))
+			for j := range n.Layers[i] {
+				fmt.Fprint(writer, "\t \t ")
+				for k := range n.Layers[i][j].Out {
+					fmt.Fprintf(writer, "%+.2f ", n.Layers[i][j].Out[k].Weight)
+				}
+				fmt.Fprint(writer, "\t \n")
 			}
-			fmt.Println()
-		}
+			fmt.Fprint(writer, "\t \t \t \n")
 
-		if i < len(n.Weights)-1 {
-			fmt.Print(len(n.Weights[i]) / len(n.Biases[i]))
-			fmt.Print(" hidden nodes with bias: ")
-			for k := range n.Biases[i+1] {
-				fmt.Printf("%+.2f ", n.Biases[i+1][k])
+		} else if i < len(n.Layers)-1 {
+			fmt.Fprintf(writer, "%v Hidden ", len(n.Layers[i]))
+			for j := range n.Layers[i] {
+				fmt.Fprintf(writer, "\t %+.2f \t ", n.Layers[i][j].Bias)
+				for k := range n.Layers[i][j].Out {
+					fmt.Fprintf(writer, "%+.2f ", n.Layers[i][j].Out[k].Weight)
+				}
+				fmt.Fprint(writer, "\t \n")
 			}
-			fmt.Print("and weight: ")
-			for j := range n.Weights[i] {
-				fmt.Printf("%+.2f ", n.Weights[i][j])
-			}
-
+			fmt.Fprint(writer, "\t \t \t \n")
 		} else {
-			fmt.Print(len(n.Weights[i]) / len(n.Biases[i]))
-			fmt.Print(" output nodes with bias: ")
-			for k := range n.Biases[i+1] {
-				fmt.Printf("%+.2f ", n.Biases[i+1][k])
-			}
-			fmt.Print("and weight: ")
-			for j := range n.Weights[i] {
-				fmt.Printf("%+.2f ", n.Weights[i][j])
+			fmt.Fprintf(writer, "%v Output ", len(n.Layers[i]))
+			for j := range n.Layers[i] {
+				fmt.Fprintf(writer, "\t %+.2f \t \t \n", n.Layers[i][j].Bias)
 			}
 		}
-		fmt.Println()
 	}
+	writer.Flush()
 }
 
 func TestForwardPropagate(t *testing.T) {
 
-	n := CreateNetwork(2, 1, []int{1}, 1)
-	n.InitDebug()
+	n, _ := createNetworkDebug(2, 3, 2)
 
-	for i := range n.Biases[0] {
-		n.Biases[0][i] = 1
+	input := []float64{1, 1}
+
+	out, err := n.ForwardPropagate(input)
+	if err != nil {
+		t.Errorf(err.Error())
 	}
 
-	out := n.ForwardPropagate()
-
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 0, ' ', tabwriter.Debug)
+	fmt.Fprint(writer, "Outputs ")
 	for o := range out {
-		fmt.Printf("%.2f ", out[o])
+		fmt.Fprintf(writer, "\t %.5f \t \n", out[o])
 	}
-	fmt.Println()
+	writer.Flush()
 }
